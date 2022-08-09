8d5848e9da6f41fd00106682028ea230f4c97e66
Joons <elddy0948@gmail.com>
2022-08-09 15:39:29 +0900

    Create Renderer and implementation ContentView

---
+++ HelloTriangle/HelloTriangle/HelloTriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTriangleApp.swift
+//  HelloTriangle
+//
+//  Created by 김호준 on 2022/08/09.
+//
+
+import SwiftUI
+
+@main
+struct HelloTriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
