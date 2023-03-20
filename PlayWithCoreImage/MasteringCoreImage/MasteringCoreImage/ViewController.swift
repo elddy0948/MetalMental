f1f499af75f56079f5146ce611a434ed3697e3d9
Joons <elddy0948@gmail.com>
2023-03-21 00:10:06 +0900

    use ciimage builtin filters

--- PlayWithCoreImage/MasteringCoreImage/MasteringCoreImage/ViewController.swift
+++ PlayWithCoreImage/MasteringCoreImage/MasteringCoreImage/ViewController.swift
@@ -1,4 +1,6 @@
 import UIKit
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 class ViewController: UIViewController {
   
@@ -8,11 +10,15 @@ class ViewController: UIViewController {
     imageView.image = UIImage(named: "sample")
     return imageView
   }()
-
+  
+  private let url = Bundle.main.url(forResource: "cat", withExtension: "JPG")!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     configureViews()
     layout()
+//    processImage()
+    builtinFilterImage()
   }
 }
 
@@ -35,6 +41,46 @@ extension ViewController {
       imageView.bottomAnchor.constraint(
         equalTo: view.safeAreaLayoutGuide.bottomAnchor),
     ])
+  }
+  
+  private func processImage() {
+    guard let image = CIImage(contentsOf: url, options: [
+      CIImageOption.applyOrientationProperty: true,
+    ]) else { return }
+    
+    guard let filter = CIFilter(name: "CIComicEffect", parameters: ["inputImage" : image]),
+          let outputImage = filter.outputImage?.applyingFilter("CIColorInvert").applyingFilter("CIVignette", parameters: [
+            "inputIntensity" : 1.0,
+            "inputRadius" : 2.0
+          ]) else {
+      return
+    }
+    
+    guard let cgImage = CIContext(options: nil).createCGImage(outputImage, from: outputImage.extent) else { return }
+    
+    imageView.image = UIImage(cgImage: cgImage)
+  }
+  
+  private func builtinFilterImage() {
+    guard let image = CIImage(contentsOf: url, options: [CIImageOption.applyOrientationProperty : true]) else { return }
+    
+    let filter = CIFilter.sepiaTone()
+    filter.inputImage = image
+    filter.intensity = 1.0
+    
+    let instantFilter = CIFilter.photoEffectInstant()
+    instantFilter.inputImage = filter.outputImage
+    
+    guard let outputImage = instantFilter.outputImage,
+          let cgImage = CIContext(options: nil).createCGImage(outputImage, from: outputImage.extent) else { return }
+    
+    let uiImage = UIImage(cgImage: cgImage)
+    imageView.image = uiImage
+    saveImageToPhotos(image: uiImage)
+  }
+  
+  private func saveImageToPhotos(image: UIImage) {
+    UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
   }
 }
 
